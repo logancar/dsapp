575aafd5c42355c4303e9b22c12bf0b2f9cf1070
Logan <logan@dent-source.com>
2025-03-26 17:23:07 -0500

    peso

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require('path');
 const fs = require('fs');
-const { fillPdf } = require('./pdfService');
+const { fillPdf } = require('./fillPdf'); // Fixed import name
 const { sendEmail } = require('./emailService');
 
 const app = express();
